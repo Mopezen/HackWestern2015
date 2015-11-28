3659d19f1f7d6da1002b6098726f129aa2cb3829
Mopezen <MeshachJ@hotmail.com>
2015-11-28 17:14:37 -0500

    Added new JS file
    The file which will post to the user browsers

---
+++ postR.js
@@ -0,0 +1,30 @@
+var poster = function(elem0,elem2,elem3,elem8,elem10,elem11,elem12,elem14,elem15,elem16){
+	var elem = [document.getElementById("adType1"),
+	document.getElementById("adType2"),
+	document.getElementById("priceType1"),
+	document.getElementById("priceAmount"),
+	document.getElementById("priceType2"),
+	document.getElementById("priceType3"),
+	document.getElementById("priceType4"),
+	document.getElementById("forsaleby_s"),
+	document.getElementById("forsaleby_s"),
+	document.getElementById("moreinfo_s"),
+	document.getElementById("postad-title"),
+	document.getElementById("pstad-descrptn"),
+	document.getElementById("pstad-map-address"),
+	document.getElementById("html5_1a56j6vdh58a1vbtpfc1nsjnpk3"),
+	document.getElementById("YoutubeURL"),
+	document.getElementById("PhoneNumber"),
+	document.getElementById("pstad-email")];
+	elem[0].checked = true;
+	elem[2].checked = true;
+	elem[3].value = 100;
+	elem[8].checked = true;
+	elem[9].selectedIndex = 3;
+	elem[10].value = "Cool Books For Sale";
+	elem[11].value = "Attempting to sell my PHP book";
+	elem[12].value = "148 Paperbirch Cres";
+	elem[14].value = "https://www.youtube.com/watch?v=9pillaOxGCo";
+	elem[15].value = "222-222-2222";
+	elem[16].value = "CoolBooks@gmail.com";
+};
