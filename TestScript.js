0e8eb411c545cab09954beb947e347e8b8a54d61
Mopezen <MeshachJ@hotmail.com>
2015-11-28 03:43:56 -0500

    Updated JS & ElementIDs

--- TestScript.js
+++ TestScript.js
@@ -1,2 +1,22 @@
-var elem = document.getElementById("adType1");
-elem.checked = checked;
+var elem = [document.getElementById("adType1"),
+document.getElementById("adType2"),
+document.getElementById("priceType1"),
+document.getElementById("priceAmount"),
+document.getElementById("priceType2"),
+document.getElementById("priceType3"),
+document.getElementById("priceType4"),
+document.getElementById("forsaleby_s"),
+document.getElementById("forsaleby_s"),
+document.getElementById("moreinfo_s"),
+document.getElementById("postad-title"),
+document.getElementById("pstad-descrptn"),
+document.getElementById("pstad-map-address"),
+document.getElementById("html5_1a56j6vdh58a1vbtpfc1nsjnpk3"),
+document.getElementById("YoutubeURL"),
+document.getElementById("PhoneNumber"),
+document.getElementById("pstad-email")];
+elem[0].checked = true;
+elem[1].checked = true;
+elem[2].value = 100;
+elem[3].checked = true;
+elem[5].value = "C:\Users\Meshach Jones\Pictures\Proof.png"
