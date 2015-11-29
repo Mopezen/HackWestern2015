4ae62c5e010fa3bc90a3403918d96862986f4d29
aalhumid <aalhumid@uwo.ca>
2015-11-28 23:42:46 -0500

    UserUI and php files done

---
+++ phpTOjs.php
@@ -0,0 +1,39 @@
+<?php
+	echo "var poster = function(elem0,ele1m,elem2,elem3,elem4,elem5,elem6,elem7,elem8,elem9,elem10,elem11,elem12,elem13,elem14,elem15){
+	var elem = [document.getElementById('adType1'),
+	document.getElementById('adType2'),
+	document.getElementById('priceType1'),
+	document.getElementById('priceAmount'),
+	document.getElementById('priceType2'),
+	document.getElementById('priceType3'),
+	document.getElementById('priceType4'),
+	document.getElementById('forsaleby_s'),
+	document.getElementById('forsaleby_s'),
+	document.getElementById('moreinfo_s'),
+	document.getElementById('postad-title'),
+	document.getElementById('pstad-descrptn'),
+	document.getElementById('pstad-map-address'),
+	document.getElementById('html5_1a56j6vdh58a1vbtpfc1nsjnpk3'),
+	document.getElementById('YoutubeURL'),
+	document.getElementById('PhoneNumber'),
+	document.getElementById('pstad-email')];
+	elem[0].checked = elem0;
+	elem[1].checked = elem1;
+	elem[2].checked = elem2;
+	elem[3].value = elem3;
+	elem[4].checked = elem0;
+	elem[5].checked = elem0;
+	elem[6].checked = elem0;
+	elem[7].checked = elem0;
+	elem[8].checked = elem8;
+	elem[9].selectedIndex = elem9;
+	elem[10].value = elem10;
+	elem[11].value = elem111;
+	elem[12].value = elem12;
+	elem[13].checked = elem0;
+	elem[14].value = elem14;
+	elem[15].value = elem15;
+	elem[16].value = elem16};
+	poster(".$_POST[postAdFormf][0].",".$_POST[postAdFormf][1].",".$_POST[postAdFormff][0].",".$_POST[postAdFormp].",".$_POST[postAdFormff][1].",".$_POST[postAdFormff][2].",".$_POST[postAdFormff][3].",".$_POST[postAdForms][0].",".$_POST[postAdForms][1].",".$_POST[postAdFormt].",".$_POST[postAdFormd].",".$_POST[postAdForm].",".$_POST[file0].",".$_POST[postAdFormy].",".$_POST[postAdFormp].",".$_POST[postAdForme].");"
+	
+?>
